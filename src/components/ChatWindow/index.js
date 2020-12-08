39ae53439bc8100246676716d715ffc6885b0a67
verissimo-sn <verissimo_sn@hotmail.com>
2020-12-08 19:10:22 -0300

    feat: body chatwindow

--- src/components/ChatWindow/index.js
+++ src/components/ChatWindow/index.js
@@ -8,31 +8,29 @@ import './styles.css';
 
 function ChatWindow() {
   return (
-    <div>
-      <div className="chatWindow">
-        <div className="chatWindow--header">
-          <div className="chatWindow--headerInfo">
-            <img className="chatWindow-avatar" src="https://greendestinations.org/wp-content/uploads/2019/05/avatar-exemple.jpg" alt=""/>
-            <div className="chatWindow-name">Nome da pessoa</div>
+    <div className="chatWindow">
+      <div className="chatWindow--header">
+        <div className="chatWindow--headerInfo">
+          <img className="chatWindow-avatar" src="https://greendestinations.org/wp-content/uploads/2019/05/avatar-exemple.jpg" alt=""/>
+          <div className="chatWindow-name">Nome da pessoa</div>
+        </div>
+        <div className="chatWindow--headerButtons">
+          <div className="chatWindow--btn">
+            <SearchIcon style={{ color: '#919191' }}/>
           </div>
-          <div className="chatWindow--headerButtons">
-            <div className="chatWindow--btn">
-              <SearchIcon style={{ color: '#919191' }}/>
-            </div>
-            <div className="chatWindow--btn">
-              <AttachFileIcon style={{ color: '#919191' }}/>
-            </div> 
-            <div className="chatWindow--btn">
-              <MoreIcon style={{ color: '#919191' }}/>
-            </div> 
-          </div>
+          <div className="chatWindow--btn">
+            <AttachFileIcon style={{ color: '#919191' }}/>
+          </div> 
+          <div className="chatWindow--btn">
+            <MoreIcon style={{ color: '#919191' }}/>
+          </div> 
         </div>
-        <div className="chatWindow--body">
+      </div>
+      <div className="chatWindow--body">
 
-        </div>
-        <div className="chatWindow--footer">
+      </div>
+      <div className="chatWindow--footer">
 
-        </div>
       </div>
     </div>
   );
