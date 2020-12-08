bc19fffd622f852ebd4959a9d85607e78393876f
verissimo-sn <verissimo_sn@hotmail.com>
2020-12-08 19:26:21 -0300

    feat: footer chatwindow

--- src/components/ChatWindow/index.js
+++ src/components/ChatWindow/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MoreIcon from '@material-ui/icons/MoreVert';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import CloseIcon from '@material-ui/icons/Close';
+import SendIcon from '@material-ui/icons/Send';
+import MicIcon from '@material-ui/icons/Mic';
 
 import './styles.css';
 
@@ -30,7 +34,19 @@ function ChatWindow() {
 
       </div>
       <div className="chatWindow--footer">
-
+        <div className="chatWindow--pre">
+          <div className="chatWindow--btn">
+            <InsertEmoticonIcon style={{ color: '#919191' }}/>
+          </div>
+        </div>
+        <div className="chatWindow--inputarea">
+          <input className="chatWindow--input" type="text" placeholder="Digite uma mensagem"/>
+        </div>
+        <div className="chatWindow--pos">
+          <div className="chatWindow--btn">
+            <SendIcon style={{ color: '#919191' }}/>
+          </div>
+        </div>
       </div>
     </div>
   );
